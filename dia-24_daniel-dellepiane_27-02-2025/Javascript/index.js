b704c5c08dcdf581de20e9d7daa9c7d97c3fdc03
DanielDellepiane <danieldellepiane96@gmail.com>
2025-03-04 12:24:16 +0100

    dia-24_daniel-dellepiane
    dia-24_daniel-dellepiane

---
+++ dia-24_daniel-dellepiane_27-02-2025/Javascript/index.js
@@ -0,0 +1,41 @@
+const parafo = document.getElementById("demo")
+// parafo.innerHTML = "Hello JS!"
+
+//console.log(parafo)
+
+//document.getElementById("demo").innerHTML = "Hello JS!"
+
+
+
+
+
+// const elem3 = document.getElementById("demo4")
+
+
+
+
+// elem3.style.color = "green";
+
+
+
+// function changeSize(newSize){
+//     elem2.style.fontSize = newSize;
+
+// }
+
+
+
+ 
+
+//Selezione di un elemento HTML
+const elem = document.getElementById("demo2")
+
+// Cambiare il colore iniziale
+elem.style.color = "blue";
+
+
+function changeColor(newColor){
+    elem.style.color = newColor;
+}
+
+ elem.innerHTML = "Hello JS!";

d5d0a6d893b32937b62f1c4aa4b9d11e2d534638
DanielDellepiane <danieldellepiane96@gmail.com>
2025-03-04 12:24:35 +0100

    dia-26_daniel-dellepiane
    dia-26_daniel-dellepiane

---
+++ dia-26_daniel-dellepiane_04-03-2025/Javascript avanzado 2/index.js
@@ -0,0 +1,103 @@
+
+// ----------------------- Ejercicio 1   IF
+
+// let a = 8;
+// let result = 'Menor a 4';
+
+// if(a > 4){
+//     result = 'Mayor a 4';
+// }
+
+// console.log(result);
+
+
+
+
+// ----------------------- Ejercicio 2    ELSE
+
+
+// const mayorEdad = 18;
+// if (mayorEdad >= 18) {
+//     console.log
+//         ("Es mayor de edad");
+// } else {
+//     console.log("Es menor de edad");
+// }
+
+
+
+
+// ----------------------- Ejercicio 3    ELSE IF
+
+
+// let x = 1;
+
+// if (x > 50) {
+//     console.log("Es mayor a 50");
+// } else if (x < 5) { console.log("Es menor a 5"); 
+
+// } else { console.log("Esta entre 5 y 50") }
+
+
+//  ----------------------- Ejercicio 4     SWITCH
+
+// const mascota = "perro";
+
+// switch (mascota) {
+//     case "perro":
+//         console.log("Tengo un perro");
+//         break;
+
+//     case "gato":
+//         console.log("Tengo un gato");
+//         break;
+
+//     case "loro":
+//         console.log("Tengo un loro");
+//         break;
+//     default:
+//         console.log("No tengo mascota");
+//         break;
+// }
+
+
+//  ----------------------- Ejercicio 5     FOR
+
+// console.log("---Ejercicio 5");
+
+// let str = '';
+
+// for (let i = 0; i < 9; i++) {
+//     str = str + i;
+// }
+
+// console.log(str);
+
+
+//  ----------------------- Ejercicio 6     WHILE
+
+// let n = 0;
+
+// while (n < 3) {
+//     n++;
+// }
+
+// console.log(n);
+
+
+//  ----------------------- Ejercicio 7     DO WHILE
+
+// let result = '';
+// let i = 0;
+// do {
+//     i = i + 1;
+//     result = result + i;
+// }
+// while (i < 5);
+
+// console.log(result);
+
+//  ----------------------- Ejercicio 8     FOR EACH
+
+const array1 = ['a','b','c']
+array1.forEach((element) => console.log(element));

8e3443b28e34d5b5ffba74a468696bfe990eac5a
DanielDellepiane <danieldellepiane96@gmail.com>
2025-03-18 15:51:29 +0100

    dia-28_daniel-dellepiane_06-03-2025
    dia-28_daniel-dellepiane_06-03-2025

---
+++ dia-28_daniel-dellepiane_06-03-2025 /Javascript avanzado 3/index.js
@@ -0,0 +1,45 @@
+
+// ----------------------- Ejercicio 1  accede a cada una de sus propiedades mostrandolas por consola.
+
+// let alumno = {
+//     nombre: 'daniel',
+//     apellido: 'dellepiane',
+// }
+
+// console.log(alumno.nombre);
+// console.log(alumno.apellido);
+
+
+// ----------------------- Ejercicio 2  Deconstruye el objeto “alumno” y accede a cada una de sus variables cargadas.
+
+// const alumno = {
+//     nombre: 'daniel',
+//     apellido: 'dellepiane',
+//     curso: 'javascript',
+// }
+
+// const { nombre, apellido, curso } = alumno;
+
+
+// const { nombre: proprio1, apellido: proprio2, curso: web } = alumno;
+
+
+// console.log(proprio1);
+
+
+
+
+// ----------------------- Ejercicio 3  Añade al objeto “alumno” una metodo. Que imprimar el nombre y apellidos del alumno por consola.
+
+let alumno1 = {
+    nombre: 'daniel',
+    apellido: 'dellepiane',
+    curso: 'javascript',
+    metodo1: function(){},
+}
+
+let alumno2 = {
+    metodo1:() => {},
+}
+
+console.log (alumno2)
